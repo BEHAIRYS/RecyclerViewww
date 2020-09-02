f4c44c50d5026e018abdebc5134304c50cdb7065
RecyclerView <ahmedmadridy.0@gmail.com>
2020-09-02 06:11:55 +0200

    finished 2 methods of viewholder

--- app/src/main/java/com/example/recyclerview/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerview/RecyclerAdapter.kt
@@ -1,12 +1,31 @@
 package com.example.recyclerview
 
+import android.provider.DocumentsContract
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recycler_view.view.*
 
-class RecyclerAdapter : RecyclerView.Adapter() {
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.viewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view,parent,
+            false)
+        return viewHolder(itemView)
+    }
+
+
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
     class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 val imageView : ImageView = itemView.imageView1
         val textView1 : TextView = itemView.textView1
