e49e287d0c7e4b2f835a81e3107231da6bdeb319
RecyclerView <ahmedmadridy.0@gmail.com>
2020-09-02 06:06:09 +0200

    View Holder constructed

--- app/src/main/java/com/example/recyclerview/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerview/RecyclerAdapter.kt
@@ -1,4 +1,15 @@
 package com.example.recyclerview
 
-class RecyclerAdapter {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recycler_view.view.*
+
+class RecyclerAdapter : RecyclerView.Adapter() {
+    class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+val imageView : ImageView = itemView.imageView1
+        val textView1 : TextView = itemView.textView1
+        val textView2 : TextView = itemView.TextView2
+    }
 }
