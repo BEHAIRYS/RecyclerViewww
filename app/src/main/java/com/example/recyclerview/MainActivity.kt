3c844b8acd49e859c9abfd57a751afed050c2e85
Mostafa Shams <70518039+MostafaShamsss@users.noreply.github.com>
2020-09-03 15:20:25 +0200

    - Main Activity Interface

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task4_movies.data.MovieResults
+import com.example.task4_movies.data.repository.MovieRepository
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MovieRepository.MovieCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    //to the recycler view activity
+    fun bindMovieData(movieResults: MovieResults)
+    {
+
+    }
+
+    override fun movieResponse(movieResults: MovieResults)
+    {
+        bindMovieData(movieResults)
+    }
+
+    override fun movieResponseError(errorMessage:String)
+    {
+
     }
 }
