37ab475874576c6a86bc3ebe9dfb5a78ffa6a8d5
Mostafa Shams <70518039+MostafaShamsss@users.noreply.github.com>
2020-09-03 15:17:57 +0200

    - Code Indentation

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // val list :
+        //recyclerView1.adapter= RecyclerAdapter(list)
+        recyclerView1.layoutManager= LinearLayoutManager(this)
+        recyclerView1.setHasFixedSize(true)
+
     }
 }
