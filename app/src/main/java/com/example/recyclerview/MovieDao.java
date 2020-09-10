7dae11bc059ae1d739f335863a0a31b0bad83a01
RecyclerView <ahmedmadridy.0@gmail.com>
2020-09-10 15:30:06 +0200

    MovieDao class done

--- app/src/main/java/com/example/recyclerview/MovieDao.java
+++ app/src/main/java/com/example/recyclerview/MovieDao.java
@@ -1,4 +1,24 @@
 package com.example.recyclerview;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface MovieDao {
+    @Insert
+    void insert(Movies movie);
+    @Update
+    void update(Movies movie);
+    @Delete
+    void delete(Movies movie);
+    @Query("Delete From movie_table")
+    void deleteAllNotes();
+    @Query("Select * From movie_table Order BY rating DESC")
+    LiveData<List<Movies>> getAllMovies();
 }
