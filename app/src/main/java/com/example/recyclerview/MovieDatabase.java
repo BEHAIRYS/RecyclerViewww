1cdad545011a8111e4b1cd587b03ca2d3f915cdf
RecyclerView <ahmedmadridy.0@gmail.com>
2020-09-10 15:34:47 +0200

    MovieDatabase class done

--- app/src/main/java/com/example/recyclerview/MovieDatabase.java
+++ app/src/main/java/com/example/recyclerview/MovieDatabase.java
@@ -1,4 +1,25 @@
 package com.example.recyclerview;
 
-public class MovieDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+@Database(entities = {Movies.class},version = 1)
+public abstract class MovieDatabase extends RoomDatabase {
+
+    private static MovieDatabase instance;
+    public abstract MovieDao noteDao();
+    public synchronized MovieDatabase getInstance(Context context){
+        if(instance== null)
+        {
+            instance= Room.databaseBuilder(context.getApplicationContext(),MovieDatabase.class,
+                    "note_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+
+    }
+
 }
